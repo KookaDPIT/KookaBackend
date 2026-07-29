9d689b1452b0eb80aa1c7d1a41fb8ca15045c925
Razvan <razvan.fanaru@gmail.com>
2026-07-29 22:55:54 +0300

    Integration of frontend in backend

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app = FastAPI(title="Cooking App API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://kookafrontend.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
